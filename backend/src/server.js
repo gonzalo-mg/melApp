05d7fa68db5761e6c638fd2e78ea86517dfa58a4
gon <gonzaloworkmail@gmail.com>
2024-12-14 10:29:27 +0100

    be/feat: implemented errorHandler middleware

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,24 +1,29 @@
 const express = require("express");
 require("dotenv").config();
-const swaggerUi = require('swagger-ui-express');
-const swaggerFile = require('./swagger_output.json');
+const swaggerUi = require("swagger-ui-express");
+const swaggerFile = require("./swagger_output.json");
+const errorHandler = require("./middlewares/errorHandler");
 
 const { PORT } = process.env;
 
-const getAllHarvestsOfUser = require('./controllers/getAllHarvestsOfUser');
+const getAllHarvestsOfUser = require("./controllers/getAllHarvestsOfUser");
 
 const app = express();
 app.use(express.json());
 
-const cors = require('cors');
+const cors = require("cors");
 app.use(cors());
 
-app.get("/", ()=>{console.log('Raíz del servidor')});
+app.get("/", () => {
+  console.log("Raíz del servidor");
+});
 
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
-app.get('/harvests', getAllHarvestsOfUser)
+app.get("/harvests", getAllHarvestsOfUser);
+
+app.use(errorHandler);
 
 app.listen(PORT, () => {
-    console.log(`API-REST melApp a la eschucha en: http://localhost:${PORT}`);
-  });
+  console.log(`API-REST melApp a la eschucha en: http://localhost:${PORT}`);
+});

af1f5597966213dafa527bd0ad9384f1c9a690b1
gon <gonzaloworkmail@gmail.com>
2024-12-15 14:46:10 +0100

    be/feat: add authValidation.js middleware to check authentication with token validity

---
+++ backend/src/middlewares/authValidation.js
@@ -0,0 +1,38 @@
+const jwt = require("jsonwebtoken");
+const createHttpError = require("../utilities/createHttpError");
+
+const authValidation = (req, res, next) => {
+  try {
+    // recuperar header authorization
+    const { authorization } = req.headers;
+
+    if (!authorization) {
+      createHttpError(
+        "Authorization header missing; can not proceed without it",
+        400
+      );
+    }
+
+    // verificar token y su formato ("Bearer xxxxx")
+    const [type, token] = authorization.split(" ");
+
+    if (type !== "Bearer" || !token) {
+      createHttpError("Invalid token format", 400);
+    }
+
+    let tokenPayload;
+    try {
+      tokenPayload = jwt.verify(token, process.env.JWT_SECRET);
+    } catch (error) {
+      createError("Invalid token", 400);
+    }
+
+    // crear propiedad de autenticacion en el objeto de peticion para el resto de middlewares y endpoints; darle los datos del token
+    req.auth = tokenPayload;
+    next();
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = authValidation;

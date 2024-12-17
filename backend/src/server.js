d5139b4cb7f18f7f8e79c4f20513f7db6f013f0a
gon <gonzaloworkmail@gmail.com>
2024-12-17 20:22:46 +0100

    be/feat: add endpoint GET/clients/:clientId

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,7 +6,7 @@ require("dotenv").config();
 const { PORT } = process.env;
 
 const cors = require("cors");
-// swagger: herramienta para documentar apis 
+// swagger: herramienta para documentar apis
 const swaggerUi = require("swagger-ui-express");
 const swaggerFile = require("./swagger_output.json");
 
@@ -19,12 +19,12 @@ const getAllSuppliersOfUser = require("./controllers/getAllSuppliersOfUser");
 const getSupplierById = require("./controllers/getSupplierById");
 // clients
 const getAllClientsOfUser = require("./controllers/getAllClientsOfUser");
+const getClientById = require("./controllers/getClientById");
 // harvests
 const getAllHarvestsOfUser = require("./controllers/getAllHarvestsOfUser");
 const getHarvestById = require("./controllers/getHarvestById");
 
 const errorHandler = require("./middlewares/errorHandler");
-
 
 // middlewares utilitarios
 app.use(express.json());
@@ -40,6 +40,7 @@ app.get("/suppliers", authValidation, getAllSuppliersOfUser);
 app.get("/suppliers/:supplierId", authValidation, getSupplierById);
 // clients
 app.get("/clients", authValidation, getAllClientsOfUser);
+app.get("/clients/:clientId", authValidation, getClientById);
 // harvests
 app.get("/harvests", authValidation, getAllHarvestsOfUser);
 app.get("/harvests/:harvestId", authValidation, getHarvestById);

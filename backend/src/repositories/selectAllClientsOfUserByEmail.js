fce119a3c93a8bc3bc7c202df7705b57b6be4718
gon <gonzaloworkmail@gmail.com>
2024-12-17 18:22:01 +0100

    be/fix: modifiy repo selectAllClientsOfUserByEmail to return array of objects; without metadata

--- backend/src/repositories/selectAllClientsOfUserByEmail.js
+++ backend/src/repositories/selectAllClientsOfUserByEmail.js
@@ -3,7 +3,11 @@ const getPool = require("../dataBase/getPool");
 async function selectAllClientsOfUserByEmail(email) {
   const pool = await getPool();
 
-  return await pool.query("select * from clients where userEmail = ?", [email]);
+  const [clients] = await pool.query(
+    "select * from clients where userEmail = ?",
+    [email]
+  );
+  return clients;
 }
 
 module.exports = selectAllClientsOfUserByEmail;

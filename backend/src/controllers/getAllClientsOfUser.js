99213dd5848690cb0f214264bba4a99cc818c81b
gon <gonzaloworkmail@gmail.com>
2024-12-17 18:24:43 +0100

    be/feat: add controller getAllClientsOfUser.js

---
+++ backend/src/controllers/getAllClientsOfUser.js
@@ -0,0 +1,27 @@
+const selectAllClientsOfUserByEmail = require("../repositories/selectAllClientsOfUserByEmail");
+
+async function getAllClientsOfUser(req, res, next) {
+  /**
+  #swagger.tags = ['Clients']
+  #swagger.description = 'Get all clients of current user.'
+
+  #swagger.security = [{
+    "bearerAuth": []
+  }]
+    
+  #swagger.parameters['$ref'] = ["#/components/parameters/userEmailHeader"]
+
+  #swagger.responses[200] = {
+    description: 'Sent clients (array of objects) to client.',
+  }
+*/
+  try {
+    const clients = await selectAllClientsOfUserByEmail(req.headers.useremail);
+
+    res.status(200).send(clients);
+  } catch (error) {
+    next(error);
+  }
+}
+
+module.exports = getAllClientsOfUser;

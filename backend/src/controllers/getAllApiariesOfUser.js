832eea66bfa1092cf825db81fc72c7e765074d6a
gon <gonzaloworkmail@gmail.com>
2024-12-17 20:30:06 +0100

    be/feat: add controller getAllApiariesOfUser

---
+++ backend/src/controllers/getAllApiariesOfUser.js
@@ -0,0 +1,25 @@
+const selectAllApiariesOfUserByEmail = require("../repositories/selectAllApiariesOfUserByEmail");
+
+async function getAllApiariesOfUser(req, res, next) {
+  /**
+  #swagger.tags = ['Apiaries']
+  #swagger.description = 'Get all apiaries of current user.'
+
+  #swagger.security = [{
+    "bearerAuth": []
+  }]
+    
+  #swagger.responses[200] = {
+    description: 'Sent apiaries (array of objects) to client.',
+  }
+*/
+  try {
+    const apiaries = await selectAllApiariesOfUserByEmail(req.userEmail);
+
+    res.status(200).send(apiaries);
+  } catch (error) {
+    next(error);
+  }
+}
+
+module.exports = getAllApiariesOfUser;

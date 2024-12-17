118c73e09bfe0b1af2ae0468da835c2051796da8
gon <gonzaloworkmail@gmail.com>
2024-12-17 12:42:12 +0100

    be/feat: implemented route for GET/harvests/:id

--- backend/src/server.js
+++ backend/src/server.js
@@ -18,6 +18,7 @@ app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 app.post("/login", loginUser);
 
 app.get("/harvests", authValidation, getAllHarvestsOfUser);
+app.get("/harvests/:id", authValidation, getAllHarvestsOfUser);
 
 app.use(errorHandler);
 

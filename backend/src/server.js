23697b9ef9df0671b881a6fe17d3fc65762927f6
gon <gonzaloworkmail@gmail.com>
2024-12-17 18:17:15 +0100

    be/feat: add enpoint GET/suppliers to server

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,25 +1,40 @@
+// archivo principal servidor express
 const express = require("express");
+const app = express();
+// variables entorno
 require("dotenv").config();
 const { PORT } = process.env;
+
 const cors = require("cors");
+// swagger: herramienta para documentar apis 
 const swaggerUi = require("swagger-ui-express");
 const swaggerFile = require("./swagger_output.json");
-const errorHandler = require("./middlewares/errorHandler");
+
+// controllers
+// users
 const authValidation = require("./middlewares/authValidation");
 const loginUser = require("./controllers/loginUser");
+// suppliers
+const getAllSuppliersOfUser = require("./controllers/getAllSuppliersOfUser");
+// harvests
 const getAllHarvestsOfUser = require("./controllers/getAllHarvestsOfUser");
 const getHarvestById = require("./controllers/getHarvestById");
 
-const app = express();
+const errorHandler = require("./middlewares/errorHandler");
+
+// middlewares utilitarios
 app.use(express.json());
 app.use(cors());
 
+// endpoints
+// documentacion de la api
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
-
+// users
 app.post("/login", loginUser);
-
+// suppliers
+app.get("/suppliers", authValidation, getAllSuppliersOfUser);
+// harvests
 app.get("/harvests", authValidation, getAllHarvestsOfUser);
-
 app.get("/harvests/:harvestId", authValidation, getHarvestById);
 
 app.use(errorHandler);

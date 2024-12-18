645d94cd6f87949e24c37ebb01ad735b705e4cf6
gon <gonzaloworkmail@gmail.com>
2024-12-18 18:41:06 +0100

    be/feat: add data schema for supplier; supplierSchema

---
+++ backend/src/dataValidationSchemas/supplierSchema.js
@@ -0,0 +1,20 @@
+const Joi = require("joi");
+const j2s = require("joi-to-swagger");
+
+const supplierSchema = Joi.object({
+  supplierId: Joi.number().integer(),
+  supplierName: Joi.string.max(100).required,
+  phone: Joi.string.max(15),
+  email: Joi.string().email().max(100),
+  web: Joi.string().max(100),
+  locality: Joi.string().max(100),
+  street: Joi.string().max(100),
+  addressNumber: Joi.integer(),
+  notes: Joi.string().max(500),
+  userEmail: Joi.string().email().max(100),
+  created: Joi.date()
+});
+
+const { swagger: supplierSchemaSwagger } = j2s(supplierSchema);
+
+module.exports = { supplierSchema, supplierSchemaSwagger };

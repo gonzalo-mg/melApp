747646369c16b70a26a1696cea4a6a3003f09f8e
gon <gonzaloworkmail@gmail.com>
2024-12-18 10:16:56 +0100

    be/feat: modify repo selectAllQueensOfUserByEmail to return single array of queens objects; without metadata

--- backend/src/repositories/selectAllQueensOfUserByEmail.js
+++ backend/src/repositories/selectAllQueensOfUserByEmail.js
@@ -3,7 +3,11 @@ const getPool = require("../dataBase/getPool");
 async function selectAllQueensOfUserByEmail(email) {
   const pool = await getPool();
 
-  return await pool.query("select * from queens where userEmail = ?", [email]);
+  const [queens] = await pool.query(
+    "select * from queens where userEmail = ?",
+    [email]
+  );
+  return queens;
 }
 
 module.exports = selectAllQueensOfUserByEmail;

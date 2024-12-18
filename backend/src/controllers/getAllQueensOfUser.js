61604a33757075af81d439e6d183e2354a9077b5
gon <gonzaloworkmail@gmail.com>
2024-12-18 10:19:21 +0100

    be/feat: add controller getAllQueensOfUser;

---
+++ backend/src/controllers/getAllQueensOfUser.js
@@ -0,0 +1,25 @@
+const selectAllClientsOfUserByEmail = require("../repositories/selectAllClientsOfUserByEmail");
+
+async function getAllClientsOfUser(req, res, next) {
+  /**
+  #swagger.tags = ['Clients']
+  #swagger.description = 'Get all clients of current user.'
+
+  #swagger.security = [{
+    "bearerAuth": []
+  }]
+    
+  #swagger.responses[200] = {
+    description: 'Sent clients (array of objects) to client.',
+  }
+*/
+  try {
+    const clients = await selectAllClientsOfUserByEmail(req.userEmail);
+
+    res.status(200).send(clients);
+  } catch (error) {
+    next(error);
+  }
+}
+
+module.exports = getAllClientsOfUser;

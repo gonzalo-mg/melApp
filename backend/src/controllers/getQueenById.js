5f2e55caa3303eeae1defee7a1191c3f13932c99
gon <gonzaloworkmail@gmail.com>
2024-12-18 10:35:40 +0100

    be/feat: add controller getQueenById

---
+++ backend/src/controllers/getQueenById.js
@@ -0,0 +1,34 @@
+const idNumSchema = require("../dataValidationSchemas/idNum");
+const selectClientById = require("../repositories/selectClientById");
+
+async function getClientById(req, res, next) {
+  /**
+  #swagger.tags = ['Clients']
+  #swagger.description = 'Get client of current user.'
+
+  #swagger.security = [{
+    "bearerAuth": []
+  }]
+    
+  #swagger.parameters['clientId] = {
+    in: 'path',                                           
+    required: 'true'
+  }
+
+  #swagger.responses[200] = {
+    description: 'Sent client (object) to client.',
+  }
+*/
+  try {
+    //validar q el id es de naturaleza numerica
+    await idNumSchema.validateAsync(parseInt(req.params.clientId, 10))
+
+    const [client] = await selectClientById(req.params.clientId, req.userEmail);
+    
+    res.status(200).send(client);
+  } catch (error) {
+    next(error);
+  }
+}
+
+module.exports = getClientById;

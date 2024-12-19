df7eccb6bc2caa1bcc1917970e317eaae7bc3698
gon <gonzaloworkmail@gmail.com>
2024-12-19 20:00:49 +0100

    be/feat: add endpoint POST/client

--- backend/src/server.js
+++ backend/src/server.js
@@ -19,9 +19,11 @@ const registerUser = require("./controllers/registerUser");
 const getAllSuppliersOfUser = require("./controllers/getAllSuppliersOfUser");
 const getSupplierById = require("./controllers/getSupplierById");
 const postSupplier = require("./controllers/postSupplier");
+const putSupplier = require("./controllers/putSupplier");
 // clients
 const getAllClientsOfUser = require("./controllers/getAllClientsOfUser");
 const getClientById = require("./controllers/getClientById");
+const postClient = require("./controllers/postClient");
 // apiaries
 const getAllApiariesOfUser = require("./controllers/getAllApiariesOfUser");
 const getApiaryById = require("./controllers/getApiaryById");
@@ -36,7 +38,7 @@ const getAllHarvestsOfUser = require("./controllers/getAllHarvestsOfUser");
 const getHarvestById = require("./controllers/getHarvestById");
 // errors
 const errorHandler = require("./middlewares/errorHandler");
-const putSupplier = require("./controllers/putSupplier");
+
 
 // middlewares utilitarios
 app.use(express.json());
@@ -56,6 +58,7 @@ app.put("/supplier", authValidation, putSupplier);
 // clients
 app.get("/clients", authValidation, getAllClientsOfUser);
 app.get("/clients/:clientId", authValidation, getClientById);
+app.post("/client", authValidation, postClient);
 // apiaries
 app.get("/apiaries", authValidation, getAllApiariesOfUser);
 app.get("/apiaries/:apiaryId", authValidation, getApiaryById);

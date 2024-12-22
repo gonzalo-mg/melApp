95776a3bc3735e1548950897212b5c34f24c3b46
gon <gonzaloworkmail@gmail.com>
2024-12-22 16:33:13 +0100

    fe/feat: add refactor mock components routing structure

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -14,31 +14,4 @@ import { SupplierslistComponent } from './supplierslist/supplierslist.component'
 })
 export class AppComponent {
   title = 'frontend';
-
-  supplier1: SupplierModel = new SupplierModel({
-    supplierId: '1',
-    supplierName: 'proveedorTest',
-    phone: '123456789',
-    email: 'proveedorTest@example.com',
-    web: 'www.proveedorTest.com',
-    locality: 'Villa Bicho',
-    street: 'Avenida de los Himenópteros',
-    addressNumber: '1',
-    notes:
-      'lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum, .',
-  });
-
-  supplier2: SupplierModel = new SupplierModel({
-    supplierId: '2',
-    supplierName: 'proveedor2',
-    phone: '123456789',
-    email: 'proveedor2@example.com',
-    web: 'www.proveedor2.com',
-    locality: 'Villa Bicho',
-    street: 'Avenida de los Himenópteros',
-    addressNumber: '2',
-    notes:
-      '22222  lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum, .',
-  });
-  suppliers: Array<SupplierModel> = [this.supplier1, this.supplier2];
 }
--- frontend/src/app/supplierslist/supplierslist.component.ts
+++ frontend/src/app/supplierslist/supplierslist.component.ts
@@ -11,5 +11,32 @@ import { SuppliercardComponent } from '../suppliercard/suppliercard.component';
   styleUrl: './supplierslist.component.css',
 })
 export class SupplierslistComponent {
-  @Input({ required: true }) suppliers!: Array<SupplierModel>;
+  //@Input({ required: true }) suppliers!: Array<SupplierModel>;
+
+  supplier1: SupplierModel = new SupplierModel({
+    supplierId: '1',
+    supplierName: 'proveedorTest',
+    phone: '123456789',
+    email: 'proveedorTest@example.com',
+    web: 'www.proveedorTest.com',
+    locality: 'Villa Bicho',
+    street: 'Avenida de los Himenópteros',
+    addressNumber: '1',
+    notes:
+      'lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum, .',
+  });
+
+  supplier2: SupplierModel = new SupplierModel({
+    supplierId: '2',
+    supplierName: 'proveedor2',
+    phone: '123456789',
+    email: 'proveedor2@example.com',
+    web: 'www.proveedor2.com',
+    locality: 'Villa Bicho',
+    street: 'Avenida de los Himenópteros',
+    addressNumber: '2',
+    notes:
+      '22222  lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum, .',
+  });
+  suppliers: Array<SupplierModel> = [this.supplier1, this.supplier2];
 }

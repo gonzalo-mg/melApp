d4834b19100e1920fd2c74a9379bfb466f7a52f6
gon <gonzaloworkmail@gmail.com>
2024-12-22 16:27:29 +0100

    fe/feat: ad route /suppliers

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { SupplierslistComponent } from './supplierslist/supplierslist.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'suppliers', component: SupplierslistComponent, title:"Proveeores"
+    }
+];

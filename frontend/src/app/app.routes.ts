81a67984f359be6f95e1f87082efb0145b21b960
gon <gonzaloworkmail@gmail.com>
2024-12-22 16:40:23 +0100

    fe/fet: add supplierform.component to edit a supplier

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,8 +1,12 @@
 import { Routes } from '@angular/router';
 import { SupplierslistComponent } from './supplierslist/supplierslist.component';
+import { SupplierformComponent } from './supplierform/supplierform.component';
 
 export const routes: Routes = [
     {
-        path: 'suppliers', component: SupplierslistComponent, title:"Proveeores"
+        path: 'suppliers', component: SupplierslistComponent, title:"Proveedores"
+    },
+    {
+        path: 'suppliers/:supplierId', component: SupplierformComponent, title:"Editar Proveedor"
     }
 ];
---
+++ frontend/src/app/supplierform/supplierform.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'melApp-supplierform',
+  imports: [],
+  templateUrl: './supplierform.component.html',
+  styleUrl: './supplierform.component.css'
+})
+export class SupplierformComponent {
+
+}

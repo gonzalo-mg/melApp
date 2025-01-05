94a97fe81b6026c07228f0d19debc82eb132e541
gon <gonzaloworkmail@gmail.com>
2025-01-05 10:39:49 +0100

    fe/frat: import httpCliente

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,14 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { DashcardComponent } from './dashcard/dashcard.component';
-
-import { SupplierModel } from './models/supplier.model';
-import { SupplierslistComponent } from './supplierslist/supplierslist.component';
 
 @Component({
   selector: 'melApp-root',
   standalone: true,
-  imports: [RouterOutlet, SupplierslistComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,10 +3,14 @@ import { SupplierslistComponent } from './supplierslist/supplierslist.component'
 import { SupplierformComponent } from './supplierform/supplierform.component';
 
 export const routes: Routes = [
-    {
-        path: 'suppliers', component: SupplierslistComponent, title:"Proveedores"
-    },
-    {
-        path: 'suppliers/:supplierId', component: SupplierformComponent, title:"Editar Proveedor"
-    }
+  {
+    path: 'suppliers',
+    component: SupplierslistComponent,
+    title: 'Proveedores',
+  },
+  {
+    path: 'suppliers/:supplierId',
+    component: SupplierformComponent,
+    title: 'Editar Proveedor',
+  },
 ];

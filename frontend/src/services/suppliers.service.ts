07d7956c7b28a680bff59d53e25d661dbc4beb54
gon <gonzaloworkmail@gmail.com>
2025-01-05 13:51:11 +0100

    fe/feat: add error handling for suppliers service

--- frontend/src/services/suppliers.service.ts
+++ frontend/src/services/suppliers.service.ts
@@ -1,7 +1,11 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable, toArray } from 'rxjs';
-import { SupplierModel } from '../models/supplier.model';
+import {
+  HttpClient,
+  HttpHeaders,
+  HttpErrorResponse,
+} from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 import { environment } from '../environments/environment';
 import { BackendResponse } from '../models/backendResponse';
 
@@ -16,11 +20,24 @@ export class SuppliersService {
   getSuppliers(): Observable<BackendResponse> {
     const headers = new HttpHeaders({
       'Content-Type': 'application/json',
-      Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyRW1haWwiOiJhYmVqYW1heWFAZW1haWwuY29tIiwiaWF0IjoxNzM2MDczOTU1LCJleHAiOjE3MzYxNjAzNTV9.bjJCMdEKM-2gPfYxRkeFixzlKLRlXKwwMljrQPz5T-w',
+      Authorization:
+        'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyRW1haWwiOiJhYmVqYW1heWFAZW1haWwuY29tIiwiaWF0IjoxNzM2MDczOTU1LCJleHAiOjE3MzYxNjAzNTV9.bjJCMdEKM-2gPfYxRkeFixzlKLRlXKwwMljrQPz5T-',
     });
-    let resp =  this.http.get<BackendResponse>(`${this.apiUrl}/suppliers`, {
-      headers,
-    });
-    return resp;
+
+    return this.http
+      .get<BackendResponse>(`${this.apiUrl}/suppliers`, { headers })
+      .pipe(catchError(this.handleError));
+  }
+
+  private handleError(error: HttpErrorResponse): Observable<never> {
+    let errorMessage = 'An error occurred.';
+    if (error.error instanceof ErrorEvent) {
+      // Client-side error
+      errorMessage = `Error: ${error.error.message}.`;
+    } else {
+      // Server-side error
+      errorMessage = `Error from server - Code: ${error.status}. Message: ${error.message}; ${error.error.message}. `;// el ultimo string es el mensaje de error especifico q envio desde el backend
+    }
+    return throwError(() => new Error(errorMessage));
   }
 }

bfd51be13e16512f856035b63838d27a25a81869
gon <gonzaloworkmail@gmail.com>
2025-01-05 12:47:57 +0100

    fe/feat: add service to get suppliers from backend; auth token injected manually

--- frontend/src/app/supplierslist/supplierslist.component.ts
+++ frontend/src/app/supplierslist/supplierslist.component.ts
@@ -1,7 +1,9 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { RouterModule, Routes, RouterOutlet } from '@angular/router';
+import { Observable } from 'rxjs';
 import { SupplierModel } from '../../models/supplier.model';
 import { SuppliercardComponent } from '../suppliercard/suppliercard.component';
+import { SuppliersService } from '../../services/suppliers.service';
 
 @Component({
   selector: 'melApp-supplierslist',
@@ -10,33 +12,23 @@ import { SuppliercardComponent } from '../suppliercard/suppliercard.component';
   templateUrl: './supplierslist.component.html',
   styleUrl: './supplierslist.component.css',
 })
-export class SupplierslistComponent {
-  //@Input({ required: true }) suppliers!: Array<SupplierModel>;
+export class SupplierslistComponent implements OnInit {
+  suppliers!: SupplierModel[];
 
-  supplier1: SupplierModel = new SupplierModel({
-    supplierId: '1',
-    supplierName: 'proveedorTest',
-    phone: '123456789',
-    email: 'proveedorTest@example.com',
-    web: 'www.proveedorTest.com',
-    locality: 'Villa Bicho',
-    street: 'Avenida de los Himenópteros',
-    addressNumber: '1',
-    notes:
-      'lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum, .',
-  });
+  constructor(private readonly suppliersService: SuppliersService) {}
 
-  supplier2: SupplierModel = new SupplierModel({
-    supplierId: '2',
-    supplierName: 'proveedor2',
-    phone: '123456789',
-    email: 'proveedor2@example.com',
-    web: 'www.proveedor2.com',
-    locality: 'Villa Bicho',
-    street: 'Avenida de los Himenópteros',
-    addressNumber: '2',
-    notes:
-      '22222  lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum, .',
-  });
-  suppliers: Array<SupplierModel> = [this.supplier1, this.supplier2];
+  ngOnInit(): void {
+    this.loadSuppliers();
+  }
+
+  loadSuppliers(): void {
+    this.suppliersService.getSuppliers().subscribe({
+      next: (response) => {
+        this.suppliers = response.payload;
+      },
+      error: (err) => {
+        console.error('Error fetching suppliers', err);
+      },
+    });
+  }
 }
--- frontend/src/services/suppliers.service.ts
+++ frontend/src/services/suppliers.service.ts
@@ -1,20 +1,26 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, toArray } from 'rxjs';
 import { SupplierModel } from '../models/supplier.model';
 import { environment } from '../environments/environment';
+import { BackendResponse } from '../models/backendResponse';
 
 @Injectable({
   providedIn: 'root',
 })
 export class SuppliersService {
-  
-  private readonly endpoint = '/suppliers'
-  private apiUrl = environment.apiUrl;
+  private readonly apiUrl = environment.apiUrl;
 
   constructor(private readonly http: HttpClient) {}
 
-  getSuppliers(): Observable<SupplierModel[]> {
-    return this.http.get<SupplierModel[]>(this.apiUrl);
+  getSuppliers(): Observable<BackendResponse> {
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyRW1haWwiOiJhYmVqYW1heWFAZW1haWwuY29tIiwiaWF0IjoxNzM2MDczOTU1LCJleHAiOjE3MzYxNjAzNTV9.bjJCMdEKM-2gPfYxRkeFixzlKLRlXKwwMljrQPz5T-w',
+    });
+    let resp =  this.http.get<BackendResponse>(`${this.apiUrl}/suppliers`, {
+      headers,
+    });
+    return resp;
   }
 }

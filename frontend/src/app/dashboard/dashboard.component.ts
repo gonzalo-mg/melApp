91b00b7d659e8745960f11865a5c8113aa23b96c
gon <gonzaloworkmail@gmail.com>
2025-01-06 17:01:54 +0100

    add dashboard component; draft

--- frontend/src/app/dashboard/dashboard.component.ts
+++ frontend/src/app/dashboard/dashboard.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+import { DashcardComponent } from '../dashcard/dashcard.component';
 
 @Component({
   selector: 'melApp-dashboard',
-  imports: [],
+  standalone: true,
+  imports: [DashcardComponent],
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.css'
+  styleUrl: './dashboard.component.css',
 })
 export class DashboardComponent {
-
+  targetPath: string = '';
+  iconPath: string = '';
+  cardTitle: string = 'Titulo';
 }

74826acb214994f87e177347c5542125790a787a
gon <gonzaloworkmail@gmail.com>
2025-01-06 15:21:29 +0100

    add route register to use RegisterComponent

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,9 +1,15 @@
 import { Routes } from '@angular/router';
+import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
 import { SupplierslistComponent } from './supplierslist/supplierslist.component';
 import { SupplierformComponent } from './supplierform/supplierform.component';
 
 export const routes: Routes = [
+  {
+    path: 'register',
+    component: RegisterComponent,
+    title: 'Register',
+  },
   {
     path: 'login',
     component: LoginComponent,

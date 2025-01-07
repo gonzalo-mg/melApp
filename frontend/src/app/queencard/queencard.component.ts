bd26a224fea51cfd9eac1bf7b884910528d38bda
gon <gonzaloworkmail@gmail.com>
2025-01-07 12:55:50 +0100

    implement queencard

--- frontend/src/app/queencard/queencard.component.ts
+++ frontend/src/app/queencard/queencard.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { QueenModel } from '../../models/queen.model';
 
 @Component({
   selector: 'melApp-queencard',
   imports: [],
   templateUrl: './queencard.component.html',
-  styleUrl: './queencard.component.css'
+  styleUrl: './queencard.component.css',
 })
 export class QueencardComponent {
-
+  @Input({ required: true }) queen!: QueenModel;
 }

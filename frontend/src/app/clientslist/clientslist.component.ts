71f00d6cd70dd00a138e38a0265d953448668e3a
gon <gonzaloworkmail@gmail.com>
2025-01-07 11:44:50 +0100

    implement clientlist component

--- frontend/src/app/clientslist/clientslist.component.ts
+++ frontend/src/app/clientslist/clientslist.component.ts
@@ -1,11 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { ClientcardComponent } from '../clientcard/clientcard.component';
+import { ClientModel } from '../../models/client.model';
+import { ClientsService } from '../../services/clients.service';
 
 @Component({
   selector: 'melApp-clientslist',
-  imports: [],
+  imports: [ClientcardComponent, RouterModule],
   templateUrl: './clientslist.component.html',
-  styleUrl: './clientslist.component.css'
+  styleUrl: './clientslist.component.css',
 })
-export class ClientslistComponent {
+export class ClientslistComponent implements OnInit {
+  clients!: ClientModel[];
 
+  constructor(private readonly clientsService: ClientsService) {}
+
+  ngOnInit(): void {
+    this.loadClients();
+  }
+
+  loadClients(): void {
+    this.clientsService.getClients().subscribe({
+      next: (response) => {
+        this.clients = response.payload;
+      },
+      error: (err) => {
+        console.error('Error recuperando clientes', err);
+      },
+    });
+  }
 }

d9ec41c4e69e5a409730b043f61457b03f1defb9
gon <gonzaloworkmail@gmail.com>
2025-01-07 15:49:07 +0100

    add harvest model

--- frontend/src/models/harvest.model.ts
+++ frontend/src/models/harvest.model.ts
@@ -1,5 +1,20 @@
 // interfaz para poder alimentar el constructor con un objeto, para q el orden sea irrelevante
-interface ApiaryResponseModel {
+interface HarvestResponseModel {
+  harvestId: string;
+  harvestDate: string;
+  kgHoney: string;
+  kgPollen: string;
+  beehiveId: string;
+  numFrames: string;
+  numSuperChambers: string;
+  numBroodChamners: string;
+  queenExcluder: string;
+  screenedBottom: string;
+  slattedRack: string;
+  landingBoard: string;
+  entranceReducer: string;
+  pollenTrap: string;
+  queenId: string;
   apiaryId: string;
   apiaryName: string;
   locality: string;
@@ -8,16 +23,31 @@ interface ApiaryResponseModel {
   nomad: string;
   vegetation: string;
   hmToWater: string;
-  startDate: string;
-  endDate: string;
-  userIsOwner: string;
+  clientId: string;
+  userEmail: string;
+  created: string;
 }
 
-export class ApiaryModel implements ApiaryResponseModel {
+export class HarvestModel implements HarvestResponseModel {
   constructor(
     // recibo el objeto de datos del backend
-    data: ApiaryResponseModel,
+    data: HarvestResponseModel,
     // asigno a propiedades de clase; sintaxis abreviada TS
+    public harvestId: string = data.harvestId,
+    public harvestDate: string = data.harvestDate,
+    public kgHoney: string = data.kgHoney,
+    public kgPollen: string = data.kgPollen,
+    public beehiveId: string = data.beehiveId,
+    public numFrames: string = data.numFrames,
+    public numSuperChambers: string = data.numSuperChambers,
+    public numBroodChamners: string = data.numBroodChamners,
+    public queenExcluder: string = data.queenExcluder,
+    public screenedBottom: string = data.screenedBottom,
+    public slattedRack: string = data.slattedRack,
+    public landingBoard: string = data.landingBoard,
+    public entranceReducer: string = data.entranceReducer,
+    public pollenTrap: string = data.pollenTrap,
+    public queenId: string = data.queenId,
     public apiaryId: string = data.apiaryId,
     public apiaryName: string = data.apiaryName,
     public locality: string = data.locality,
@@ -26,8 +56,8 @@ export class ApiaryModel implements ApiaryResponseModel {
     public nomad: string = data.nomad,
     public vegetation: string = data.vegetation,
     public hmToWater: string = data.hmToWater,
-    public startDate: string = data.startDate,
-    public endDate: string = data.endDate,
-    public userIsOwner: string = data.userIsOwner
+    public clientId: string = data.clientId,
+    public userEmail: string = data.userEmail,
+    public created: string = data.created
   ) {}
 }

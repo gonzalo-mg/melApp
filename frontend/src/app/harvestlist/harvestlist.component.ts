f09026cb7fab07fc303311dcfb7c8f6561eab2f0
gon <gonzaloworkmail@gmail.com>
2025-01-07 16:09:31 +0100

    implement harvest list

--- frontend/src/app/harvestlist/harvestlist.component.ts
+++ frontend/src/app/harvestlist/harvestlist.component.ts
@@ -1,11 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { HarvestcardComponent } from '../harvestcard/harvestcard.component';
+import { HarvestModel } from '../../models/harvest.model';
+import { HarvestsService } from '../../services/harvests.service';
 
 @Component({
   selector: 'melApp-harvestlist',
-  imports: [],
+  imports: [RouterModule, HarvestcardComponent],
   templateUrl: './harvestlist.component.html',
-  styleUrl: './harvestlist.component.css'
+  styleUrl: './harvestlist.component.css',
 })
-export class HarvestlistComponent {
+export class HarvestlistComponent implements OnInit {
+  harvests!: HarvestModel[];
 
+  constructor(private readonly harvestService: HarvestsService) {}
+
+  ngOnInit(): void {
+    this.loadHarvests();
+  }
+
+  loadHarvests(): void {
+    this.harvestService.getHarvests().subscribe({
+      next: (response) => {
+        this.harvests = response.payload;
+      },
+      error: (err) => {
+        console.error('Error recuperando clientes', err);
+      },
+    });
+  }
 }

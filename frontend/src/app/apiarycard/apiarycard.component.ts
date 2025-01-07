dbe1336486fb6b0cff473f3ef4c99ebe7a116bd1
gon <gonzaloworkmail@gmail.com>
2025-01-07 14:29:53 +0100

    implement ApiaryCard component

--- frontend/src/app/apiarycard/apiarycard.component.ts
+++ frontend/src/app/apiarycard/apiarycard.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { ApiaryModel } from '../../models/apiary.model';
 
 @Component({
   selector: 'melApp-apiarycard',
   imports: [],
   templateUrl: './apiarycard.component.html',
-  styleUrl: './apiarycard.component.css'
+  styleUrl: './apiarycard.component.css',
 })
 export class ApiarycardComponent {
-
+  @Input({ required: true }) apiary!: ApiaryModel;
 }

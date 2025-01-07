4dca7f086e51df3ef7680cf61955d6291de1007f
gon <gonzaloworkmail@gmail.com>
2025-01-07 16:02:48 +0100

    implement harvestcard

--- frontend/src/app/harvestcard/harvestcard.component.ts
+++ frontend/src/app/harvestcard/harvestcard.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { HarvestModel } from '../../models/harvest.model';
 
 @Component({
   selector: 'melApp-harvestcard',
   imports: [],
   templateUrl: './harvestcard.component.html',
-  styleUrl: './harvestcard.component.css'
+  styleUrl: './harvestcard.component.css',
 })
 export class HarvestcardComponent {
-
+  @Input({ required: true }) harvest!: HarvestModel;
 }

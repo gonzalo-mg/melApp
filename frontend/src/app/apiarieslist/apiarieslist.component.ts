e19e2b4e25c284d184281f77b55411926ffa0921
gon <gonzaloworkmail@gmail.com>
2025-01-07 14:41:29 +0100

    implement apiarieslist

--- frontend/src/app/apiarieslist/apiarieslist.component.ts
+++ frontend/src/app/apiarieslist/apiarieslist.component.ts
@@ -1,11 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { ApiariesService } from '../../services/apiaries.service';
+import { ApiaryModel } from '../../models/apiary.model';
+import { ApiarycardComponent } from '../apiarycard/apiarycard.component';
 
 @Component({
   selector: 'melApp-apiarieslist',
-  imports: [],
+  imports: [RouterModule, ApiarycardComponent],
   templateUrl: './apiarieslist.component.html',
   styleUrl: './apiarieslist.component.css'
 })
 export class ApiarieslistComponent {
+  apiaries!: ApiaryModel[];
 
+  constructor(private readonly apiariesService: ApiariesService) {}
+
+  ngOnInit(): void {
+    this.loadApiaries();
+  }
+  loadApiaries(): void {
+    this.apiariesService.getApiaries().subscribe({
+      next: (response) => {
+        this.apiaries = response.payload;
+      },
+      error: (err) => {
+        console.error('Error recupernado apiarios:', err);
+      },
+    });
+  }
 }

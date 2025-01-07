c5f34825487b17cbe87ec0908aeaa4495de41a40
gon <gonzaloworkmail@gmail.com>
2025-01-07 15:51:59 +0100

    add harvest card

---
+++ frontend/src/app/harvestcard/harvestcard.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'melApp-harvestcard',
+  imports: [],
+  templateUrl: './harvestcard.component.html',
+  styleUrl: './harvestcard.component.css'
+})
+export class HarvestcardComponent {
+
+}

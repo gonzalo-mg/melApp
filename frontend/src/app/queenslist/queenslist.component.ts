e210a28e205a87db67b40386ac3ed5d41ef27bc0
gon <gonzaloworkmail@gmail.com>
2025-01-07 13:51:08 +0100

    implement queens list page

--- frontend/src/app/queenslist/queenslist.component.ts
+++ frontend/src/app/queenslist/queenslist.component.ts
@@ -1,11 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+import { QueencardComponent } from '../queencard/queencard.component';
+import { QueenModel } from '../../models/queen.model';
+import { QueensService } from '../../services/queens.service';
 
 @Component({
   selector: 'melApp-queenslist',
-  imports: [],
+  imports: [RouterModule, QueencardComponent],
   templateUrl: './queenslist.component.html',
-  styleUrl: './queenslist.component.css'
+  styleUrl: './queenslist.component.css',
 })
-export class QueenslistComponent {
+export class QueenslistComponent implements OnInit {
+  queens!: QueenModel[];
 
+  constructor(private readonly queensService: QueensService) {}
+
+  ngOnInit(): void {
+    this.loadQueens();
+  }
+  loadQueens(): void {
+    this.queensService.getQueens().subscribe({
+      next: (response) => {
+        this.queens = response.payload;
+      },
+      error: (err) => {
+        console.error('Error recupernado raíñas:', err);
+      },
+    });
+  }
 }
